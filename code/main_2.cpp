d6a090ae403a406423438a9a7624ef6140870e18
Michael Conti <michaelconti@uri.edu>
2026-05-21 14:17:13 -0400

    code

---
+++ code/main_2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(){
+
+    int a, b;
+
+    // std::cout << "Enter a number for a " << std::endl;
+
+    std::cin >> a;
+
+    // std::cout << "Enter a number for b " << std::endl;
+
+    std::cin >> b;
+
+    std::cout << a << " + " << b << " = " << a + b;
+
+
+    return 0;
+}
